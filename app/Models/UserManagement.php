8355ce18d35fd12586486a9c662a1020ed5cbcd5
Sanduni Abeyrathna <68677122+SanduniAbeyrathna@users.noreply.github.com>
2022-12-16 19:26:42 +0530

    Update UserManagement.php file

--- app/Models/UserManagement.php
+++ app/Models/UserManagement.php
@@ -7,5 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class UserManagement extends Model
 {
-    use HasFactory;
+    protected $table = 'user';
+    protected $primaryKey = 'id';
+    protected $fillable = ['name', 'address', 'mobile'];
 }
